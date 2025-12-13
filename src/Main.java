ac59647fbc7891ed43bb549832ca6177ccf9c043
chahinezou2005-ops <chahinez.ou2005@gmail.com>
2025-12-13 20:27:39 +0100

    correction du l'executable car il marche pas la dernier fois

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,22 @@
-import java.nio.file.*;
 import java.util.Scanner;
-import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Nom du fichier à tester (ex: test1_correct.txt | tests/test2_for.txt | test3_incorrect.txt | test5_myname.txt | test_vide.txt) : ");
-        String fichier = sc.nextLine().trim();
 
+        System.out.println("=== MINI COMPILATEUR (while en phython) ===");
+        System.out.println("Entrez le  code puis appuyez sur Entrée :");
+        System.out.println("(Terminez avec Ctrl + Z puis Entrée)");
+        System.out.print("> ");
+
+StringBuilder codeBuilder = new StringBuilder();
+
+while (sc.hasNextLine()) {
+    codeBuilder.append(sc.nextLine()).append("\n");
+}
+
+String code = codeBuilder.toString().trim();
         try {
-            // obtient le dossier contenant le .class/.jar
-            String jarPath = new File(Main.class.getProtectionDomain()
-                    .getCodeSource()
-                    .getLocation()
-                    .toURI())
-                    .getParent();
-
-            Path testFile = Paths.get(jarPath, "tests", fichier);
-
-            String code = Files.readString(testFile);
-
             System.out.println("\n=== ANALYSE LEXICALE ===\n");
             Lexer lexer = new Lexer(code);
             lexer.analyser();
@@ -30,9 +27,8 @@ public class Main {
             parser.Z();
 
         } catch (Exception e) {
-            System.out.println("ERREUR : Fichier non trouvé ou autre erreur !");
-            System.out.println("Détail : " + e.getClass().getSimpleName() + " -> " + e.getMessage());
-            System.out.println("Assure-toi que le dossier 'tests' est dans le même dossier que le .jar");
+            System.out.println("ERREUR lors de l'analyse !");
+            System.out.println("Détail : " + e.getMessage());
         } finally {
             sc.close();
         }
